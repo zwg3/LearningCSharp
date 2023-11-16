d70c51c89af8aea8945b9716649bf25149dab7d9
zwg3333 <60962298+zwg3333@users.noreply.github.com>
2023-11-16 23:49:10 +0300

    Add project files.

---
+++ Lab01/Program.cs
@@ -0,0 +1,14 @@
+﻿class AreaCalculator
+{
+    static void Main()
+    {
+        double p, side, pHalved, result;
+
+        Console.WriteLine("Please input the equilateral triangle perimeter");
+        p = Convert.ToDouble(Console.ReadLine());
+        side = p / 3;
+        pHalved = p / 2;
+        result = Math.Sqrt(pHalved * (pHalved - side) * (pHalved - side) * (pHalved - side));
+        Console.WriteLine("Side:\t\tArea:\n" + "{0:0.00}\t\t{1:0.00}", side, result);
+    }
+}
