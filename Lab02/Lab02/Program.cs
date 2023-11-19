7a90c13947c0ec5c0e0aa527d2a345e0cb8b4b8a
zwg3333 <60962298+zwg3333@users.noreply.github.com>
2023-11-19 16:13:24 +0300

    refactoring of Lab02

--- Lab02/Lab02/Program.cs
+++ Lab02/Lab02/Program.cs
@@ -1,27 +1,38 @@
-﻿public struct Dictance
+﻿public struct Distance
 {
-    public int varA;
-    public int varB;
-    public int varC;
+    public int inch;
+    public int foot;   
 }
 
 class Program
 {
     static void Main()
     {
-        Dictance calc;
+        Distance firstSegment;
+        Distance secondSegment;
+        Distance thirdSegment;
 
-        Console.WriteLine("Please input the first distance parameter");
-        calc.varA = Int32.Parse(Console.ReadLine());
-        Console.WriteLine("Please input the second distance parameter");
-        calc.varB = Int32.Parse(Console.ReadLine());
-        calc.varC = calc.varA + calc.varB;
+        Console.WriteLine("Please input the first segment data");
+        int firstData = int.Parse(Console.ReadLine());
 
-        int feet = (int)calc.varC / 12;
-        int inches = (int)calc.varC % 12;
+        Console.WriteLine("Please input the second segment data");
+        int secondData = int.Parse(Console.ReadLine());
 
-        Console.WriteLine("The calculated distance is {0} '- {1}", feet, inches);
+        firstSegment.foot = firstData / 12;
+        firstSegment.inch = firstData % 12;
+        secondSegment.foot = secondData / 12;
+        secondSegment.inch = secondData % 12;
+
+        thirdSegment.foot = firstSegment.foot + secondSegment.foot;
+        thirdSegment.inch = firstSegment.inch + secondSegment.inch;
+
+        if (thirdSegment.inch > 12)
+        {
+            thirdSegment.foot++;
+            thirdSegment.inch = thirdSegment.inch % 12;
+        }
+        else{}
+
+        Console.WriteLine("The sum of both segments is: {0} '- {1}", thirdSegment.foot, thirdSegment.inch);
     }
-
-
 }
